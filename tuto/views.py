507ea0c8bbc44e431439f47aab60cfb0270294f2
Juliann MERIT <juliann.merit@gmail.com>
2023-09-26 14:13:03 +0200

    add all

--- tuto/views.py
+++ tuto/views.py
@@ -1,9 +1,20 @@
 from .app import app
 from flask import render_template
+from .models import get_sample
+
 
 @app.route("/")
 def home():
     return render_template(
         "home.html",
-        title = "Hello World !",
-        names = ["Pierre", "Paul", "Corinne"])
+        title="Hello World !",
+        books=get_sample())
+
+
+@app.route("/detail/<id>")
+def detail(id):
+    books = get_sample()
+    book = books[int(id)]
+    return render_template(
+        "detail.html",
+        book=book)
